b43f0acb722fbb64358ab140f2e3c749ca1923b8
RizzlerofOZ-901 <juandsalas22@gmail.com>
2026-07-30 15:45:35 -0600

    Responsive in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,14 +8,14 @@ import {Button} from "./ui/button"
 export default function Navbar(){
     return(
         <nav className="border-b border-gray-200 bg-white">
-            <div className="container mx-auto flex h-16 items-center px-4 justify-between">
+            <div className="container mx-auto flex flex-col gap-4 px-4 py-4 sm:h-16 sm:flex-row sm:items-center sm:justify-between sm:py-0">
 {/*Left side*/} 
-                <Link href='/' className="flex items-center gap-2 text-xl font-semibold">
+                <Link href='/' className="flex items-center justify-center gap-2 text-xl font-semibold sm:justify-start">
                     <Image src="/gallery/Logo.png" alt="Logo" width={100} height={100} className="h-10 w-auto"/>
                     <span>Vision M</span>
                 </Link>
 {/*Right side*/}
-                <div>
+                <div className="flex flex-wrap justify-center gap-2">
                     <Link href='/servicios'>
                         <Button variant="ghost" className="text-gray-700 hover:bg-[#FFBF00]">Sevicios</Button>
                     </Link>
